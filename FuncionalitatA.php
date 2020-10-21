fcbe4502f170aa8c56a827e8d0f6dde239354af5
Roger <rogerolivares30@gmail.com>
2020-10-21 12:07:54 +0200

    Bug prova Practica 1

---
+++ FuncionalitatA.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='es'>
+   <head>
+      <title>Bucles for</title>
+      <style>
+        td {
+            border: 1px double black;
+        }
+      </style>
+   </head>
+   <body>
+   <table style="border: 1px double black;">
+      <?php
+      $j = "0";
+        echo "<tr>";
+          for ($x = 0; $x <= 4; $x++) {
+              
+            }
+        echo "</tr>";
+      ?>
+    </table>
+   </body>
+</html>
